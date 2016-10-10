6bb3744cd201a3ac3a0ba844c7f85f5f03903482
Oscar Martin Herrera <omartin@gamma36.aulas.gsyc.urjc.es>
2016-10-10 12:59:41 +0200

    Practica 3 ejercicio 4.5

--- karaoke.py
+++ karaoke.py
@@ -7,6 +7,8 @@ from smallsmilhandler import SmallSMILHandler
 
 import sys
 import json
+import urllib
+
 
 
 def print_lista(lista):
@@ -19,6 +21,27 @@ def print_lista(lista):
         else:
             etiq = linea
 
+def URL_finder(lista):
+
+    URL_l = []
+
+    for linea in lista:
+        if isinstance(linea, dict):
+            for elem in linea:
+                if elem == "src":
+                   URL_l.append(linea[elem])
+    return URL_l
+ 
+
+def URL_files(u_l):
+
+    for linea in u_l:
+        f_l = linea.split("/")
+        url_file = f_l[-1]
+        try:
+            urllib.request.urlretrieve(linea, url_file)
+        except ValueError:
+            sys.exit("Not URL file")
 
 if __name__ == "__main__":
 
@@ -35,6 +58,9 @@ if __name__ == "__main__":
 
     print_lista(lista)
 
+    URL_list = URL_finder(lista)
+    URL_files(URL_list)
+    
     fich_json = open('karaoke.json', 'w')
     json.dump(lista, fich_json, sort_keys=True, indent=4, separators=(',', ':'))
     fich_json.close()
