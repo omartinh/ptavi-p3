a1a04b115c55303e6be34190af6a75ca29fdcffa
Oscar Martin Herrera <omartin@gamma36.aulas.gsyc.urjc.es>
2016-10-10 10:21:25 +0200

    Practica3 Ejercicio 4.4 salida etiquetas y atributos tabulados

---
+++ karaoke.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+from smallsmilhandler import SmallSMILHandler
+
+import sys
+
+
+def print_lista(lista):
+    for linea in lista:
+        atrib = ""
+        if isinstance(linea, dict):
+            for elem in linea:
+                atrib = atrib + elem + "=" + linea[elem] + "\t"
+            print(etiq + "\t" + atrib)
+        else:
+            etiq = linea
+
+
+if __name__ == "__main__":
+
+    try:
+        fich = sys.argv[1]
+    except:
+        sys.exit("Usage: Python3 karaoke.py file.smil")
+
+    parser = make_parser()
+    cHandler = SmallSMILHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open(fich))
+    lista = SmallSMILHandler.get_tags(cHandler)
+
+    print_lista(lista)
